3731079b16d609570dcd368c0ba7e0b0cf30d346
Simon Martyr <simartyr@googlemail.com>
2017-09-16 22:02:29 +0200

    removed comment

--- Feedback/Feedback/MainPage.xaml.cs
+++ Feedback/Feedback/MainPage.xaml.cs
@@ -24,11 +24,6 @@ namespace Feedback
       IList < SprinterConfigs > configs = new List<SprinterConfigs>();
       configs.Add(conf);
       await Feedback.RequestFeedback(configs);
-      //var result = await Feedback.PostXMLAsync("3ab1eb8b-fdff-4c18-a73c-313d9e7843fd", "65d6a1b9-edee-4048-9c6c-b079ae393374");
-      //Console.WriteLine(result);
-      //XmlDocument xmlDoc = new XmlDocument();
-      //xmlDoc.LoadXml(result);
-      //var issues = xmlDoc.GetElementsByTagName("Issues");
 
       base.OnAppearing();
     }
